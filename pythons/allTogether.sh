950b7019a1051ceba9a9b9c913da4703e04e9cb1
canburakt <canbu@canbu.info>
2015-12-26 02:48:10 +0200

    gitignore modified

--- pythons/allTogether.sh
+++ pythons/allTogether.sh
@@ -1,8 +1,6 @@
-#python /home/pi/Desktop/sayisal/pythons/getResult.py
+python /home/pi/Desktop/sayisal/pythons/getResult.py
 python /home/pi/Desktop/sayisal/pythons/trimResults.py
-#python /home/pi/Desktop/sayisal/pythons/getNgrams.py > /home/pi/Desktop/sayisal/sonuclarTrimmed/bestWorst.txt
-$NOW =$(date +"%Y%m%d")
-echo $NOW
+python /home/pi/Desktop/sayisal/pythons/getNgrams.py > /home/pi/Desktop/sayisal/sonuclarTrimmed/bestWorst.txt
 git add .
 git commit -m $(date +"%Y%m%d")
 git push
