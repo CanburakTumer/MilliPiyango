355aee7a97fa0464b725117017ccef3b92fdc8c1
canburakt <canbu@canbu.info>
2015-12-26 02:39:19 +0200

    $NOW

--- pythons/allTogether.sh
+++ pythons/allTogether.sh
@@ -1,7 +1,7 @@
 #python /home/pi/Desktop/sayisal/pythons/getResult.py
 python /home/pi/Desktop/sayisal/pythons/trimResults.py
 #python /home/pi/Desktop/sayisal/pythons/getNgrams.py > /home/pi/Desktop/sayisal/sonuclarTrimmed/bestWorst.txt
-$NOW =$(date+"%Y%m%d")
+$NOW =$(date +"%Y%m%d")
 echo $NOW
 git add .
 git commit -m '$NOW'
